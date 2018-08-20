86fd3db31b419aaa953d4e4b42c248995d346880
Moritz Morgenroth <development@moritzmorgenroth.de>
2018-08-20 13:26:57 +0200

    Fix imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import merge from 'lodash/merge';
 import buildDataProvider from 'ra-data-graphql';
-import { DELETE, DELETE_MANY, UPDATE, UPDATE_MANY } from 'react-admin';
+import { DELETE, DELETE_MANY, UPDATE, UPDATE_MANY, GET_LIST, GET_ONE, GET_MANY, GET_MANY_REFERENCE, CREATE} from 'react-admin';
 
 import buildQuery from './buildQuery';
 
